e44230000f1fd15ca5cece34767182dd807e24d8
Indika Wijesooriya <indikman@gmail.com>
2019-03-24 07:17:18 +0530

    Added basic elements

---
+++ DropdownSearch/Assets/DropDownSearch/Scripts/DropDownSearchItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DropDownSearchItem : MonoBehaviour
+{
+
+    public TextMeshProUGUI tex_name;
+
+    //Private variables
+    private string name;
+    private string data;
+
+    public void setData(string name, string data){
+        this.name = name;
+        this.data = data;
+        tex_name.text = name;
+    }
+
+    public string getData(){
+        return data;
+    }
+}
